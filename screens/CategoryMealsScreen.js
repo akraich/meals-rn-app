07d8a393f5f2f5a8148178bc295464ff2fa588c8
SimoJS <mohamed.akraich@gmail.com>
2019-09-09 16:23:09 +0100

    Finishing up and styling the CategoryMealScreen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,21 +1,31 @@
 import React from "react";
 
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
-import { CATEGORIES } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
 const CategoryMealsScreen = props => {
   const categoryId = props.navigation.getParam("categoryId");
-  const category = CATEGORIES.find(cat => cat.id === categoryId);
+  const meals = MEALS.filter(meal => meal.categoryIds.indexOf(categoryId) >= 0);
+  const renderListItem = itemData => (
+    <MealItem
+      title={itemData.item.title}
+      duration={itemData.item.duration}
+      complexity={itemData.item.complexity.toUpperCase()}
+      affordability={itemData.item.affordability.toUpperCase()}
+      imageUrl={itemData.item.imageUrl}
+      onSelect={() => {}}
+    />
+  );
   return (
-    <View style={styles.screen}>
-      <Text>The Category Meals Screen</Text>
-      <Text>{category.title}</Text>
-      <Button
-        title="Go to meal detail"
-        onPress={() => props.navigation.navigate({ routeName: "MealDetail" })}
-      />
-    </View>
+    <FlatList
+      style={{ width: "100%" }}
+      keyExtractor={(item, index) => item.id}
+      data={meals}
+      renderItem={renderListItem}
+    />
   );
 };
 
