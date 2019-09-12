49ba4a59d20ccaa40814a38ef91b6d4a3a99a772
SimoJS <mohamed.akraich@gmail.com>
2019-09-12 16:59:03 +0100

    Implementing the FiltersScreen and MealDetail Screen screens

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
 
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Switch, StyleSheet } from "react-native";
+import { Colors } from "../constants/Colors";
+
+const FilterSWitch = props => (
+  <View style={styles.filterContainer}>
+    <Text>{props.label}</Text>
+    <Switch
+      trackColor={{ true: Colors.primary }}
+      thumbColor={Colors.primary}
+      value={props.state}
+      onValueChange={props.onChange}
+    />
+  </View>
+);
 
 const FiltersScreen = props => {
+  [isGlutenFree, setIsGlutenFree] = useState(false);
+  [isLactoseFree, setIsLactoseFree] = useState(false);
+  [isVegan, setIsVegan] = useState(false);
+  [isVegetarian, setIsVegetarian] = useState(false);
   return (
     <View style={styles.screen}>
-      <Text>The Filters Screen</Text>
+      <Text style={styles.title}>Available Filters/Restrictions</Text>
+      <FilterSWitch
+        label="Gluten-free"
+        state={isGlutenFree}
+        onChange={newValue => setIsGlutenFree(newValue)}
+      />
+      <FilterSWitch
+        label="Lactose-free"
+        state={isLactoseFree}
+        onChange={newValue => setIsLactoseFree(newValue)}
+      />
+      <FilterSWitch
+        label="Vegan"
+        state={isVegan}
+        onChange={newValue => setIsVegan(newValue)}
+      />
+      <FilterSWitch
+        label="Vegetarian"
+        state={isVegetarian}
+        onChange={newValue => setIsVegetarian(newValue)}
+      />
     </View>
   );
 };
@@ -33,8 +70,19 @@ FiltersScreen.navigationOptions = navigationData => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center"
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    textAlign: "center",
+    margin: 20
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+    marginVertical: 10
   }
 });
 
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,23 +1,40 @@
 import React from "react";
 
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
 
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
 import HeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
 
 import { MEALS } from "../data/dummy-data";
+
+const ListItem = props => (
+  <View style={styles.listItem}>
+    <DefaultText>{props.children}</DefaultText>
+  </View>
+);
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam("mealId");
   const meal = MEALS.find(meal => meal.id === mealId);
   return (
-    <View style={styles.screen}>
-      <Text>{meal.title}</Text>
-      <Button
-        title="Go back to categories"
-        onPress={() => props.navigation.popToTop()}
-      />
-    </View>
+    <ScrollView>
+      <Image source={{ uri: meal.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <DefaultText>{meal.duration}m</DefaultText>
+        <DefaultText>{meal.complexity}</DefaultText>
+        <DefaultText>{meal.affordability}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {meal.ingredients.map(ing => (
+        <ListItem key={ing}>{ing}</ListItem>
+      ))}
+      <Text style={styles.title}>Steps</Text>
+      {meal.steps.map(step => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+    </ScrollView>
   );
 };
 
@@ -45,6 +62,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  details: {
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "space-around"
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    textAlign: "center"
+  },
+  image: {
+    width: "100%",
+    height: 200
+  },
+  listItem: {
+    marginVertical: 5,
+    marginHorizontal: 15,
+    padding: 5,
+    borderColor: "#ccc",
+    borderWidth: 1
   }
 });
 
