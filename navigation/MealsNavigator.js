531e7f66cd9972dcd0b474b540b9c6084831ebc4
SimoJS <mohamed.akraich@gmail.com>
2019-09-12 12:58:38 +0100

    Adding Bottom Tabs-based Navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,17 @@
+import React from "react";
+
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import FavoritesScreen from "../screens/FavoritesScreen";
+
+import { Ionicons } from "@expo/vector-icons";
 
 import { Colors } from "../constants/Colors";
 
@@ -26,4 +33,37 @@ const StackNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(StackNavigator);
+const routeConfigs = {
+  Meals: {
+    screen: StackNavigator,
+    navigationOptions: {
+      tabBarIcon: tabInfo => (
+        <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+      ),
+      tabBarColor: Colors.primary
+    }
+  },
+  Favorites: {
+    screen: FavoritesScreen,
+    navigationOptions: {
+      tabBarIcon: tabInfo => (
+        <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+      ),
+      tabBarColor: Colors.secondary
+    }
+  }
+};
+
+const BottomTabNavigator =
+  Platform.OS === "android"
+    ? createMaterialBottomTabNavigator(routeConfigs, {
+        activeColor: "white",
+        shifting: true
+      })
+    : createBottomTabNavigator(routeConfigs, {
+        tabBarOptions: {
+          activeTintColor: Colors.secondary
+        }
+      });
+
+export default createAppContainer(BottomTabNavigator);
